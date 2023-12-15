e1b008a236f8b6255047434b752fd00b808d2c13
Michael Pocino <michael_pocino@bose.com>
2023-12-15 12:41:48 -0500

    [MUNE-289] Refactor IIR filters
    The IIR filter code was refactored to make it easier to reuse.
    The coefficient and state memory is now allocated by an `IirFilter`
    class.  This takes reduces code in the algorithms using IIR
    filters, and hides some of the implementation-specific details,
    such as the alignment of memory or the order of coefficients.
    
    Various filter design methods are also integrated into the IIR
    filter.  New design methods can easily be added and registered
    for use by any algorithm that uses IIR filters.  See
    `filter_design.cpp` for examples.
    
    The filter design methods support filters that require multiple
    second-order sections (such as a Butterworth LPF).  These have
    not yet been tested.

--- apps/firmware-apps/fusion-dsp/src/iir.h
+++ apps/firmware-apps/fusion-dsp/src/iir.h
@@ -1,143 +1,119 @@
 #pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+
 namespace filter {
 
-/// The order in which a second-order section's a1 coefficient appears in a
-/// `coeffs` array.  See `iir_process()`
-const int A1_INDEX = 0;
 
-/// The order in which a second-order section's a2 coefficient appears in a
-/// `coeffs` array.  See `iir_process()`
-const int A2_INDEX = 1;
-
-/// The order in which a second-order section's b1 coefficient appears in a
-/// `coeffs` array.  See `iir_process()`
-const int B1_INDEX = 2;
-
-/// The order in which a second-order section's b2 coefficient appears in a
-/// `coeffs` array.  See `iir_process()`
-const int B2_INDEX = 3;
-
-
-/// Calculate the index of the first (a1) coefficient for a second-order
-/// section. See `iir_process()`.
+/// An IIR filter that supports multiple second-order sections and multiple
+/// channels.  The same filter is applied to each channel.
 ///
-/// \param section  The index of this section.
-/// \param num_sections  The total number of sections.
-/// \return  The index of this section's a1 coefficient within a `coeffs` array.
-
-static inline int coeff_index_start(int section, int num_sections)
+/// The filter coefficients can be set either directly (using
+/// `set_section_coeffs()`), or by using one of the design methods
+/// (`design_band()` or `design()`).
+class IirFilter
 {
-    if (section < (num_sections & ~0x3))
+public:
+    /// Create a multi-channel filter.
+    ///
+    /// \param  num_sections  The number of second-order sections in the filter.
+    /// \param  num_channels  The number of channels in the filter.
+    IirFilter(int num_sections, int num_channels);
+
+
+    /// Create a single-channel filter.
+    ///
+    /// \param  num_sections  The number of second-order sections in the filter.
+    IirFilter(int num_sections) : IirFilter(num_sections, 1) {}
+
+
+    /// Process one frame of audio, for single-channel filters.
+    ///
+    /// \param  out  The array of output samples, of length `frame_size`.
+    /// \param  in  The array of input samples, of length `frame_size`.
+    /// \param  frame_size  The number of samples to be processed.
+    void process(float *out, const float *in, int frame_size)
     {
-        return 16 * (section >> 2) + (section & 0x3);
+        process_impl(out, in, 0, frame_size);
     }
-    else if (section < (num_sections & ~0x1))
+
+
+    /// Process one frame of audio, for multi-channel filters.  This applies
+    /// the same filter to each channel.
+    ///
+    /// \param  out  A vector of arrays of output samples, each of length
+    ///             `frame_size`.
+    /// \param  in  A vector of arrays of input samples, each of length
+    ///            `frame_size`.
+    /// \param  frame_size  The number of samples to be processed.
+    void process(std::vector<float *> &out, std::vector<const float *> &in,
+                 int frame_size)
     {
-        return 16 * (num_sections >> 2) + (section & 0x3);
+        for (int channel = 0; channel < num_channels; ++channel)
+        {
+            process_impl(out[channel], in[channel], channel, frame_size);
+        }
     }
-    else
-    {
-        return 16 * (num_sections >> 2) + 4 * (num_sections & 0x2);
-    }
-}
 
 
-/// Calculate the index offset between coefficients of a second-order section.
-/// See `iir_process()`.
-///
-/// \param section  The index of this section.
-/// \param num_sections  The total number of sections.
-/// \return  The index stride between this section's coefficients within a
-///     `coeffs` array.
-
-static inline int coeff_index_stride(int section, int num_sections)
-{
-    if (section < (num_sections & ~0x3))
-    {
-        return 4;
-    }
-    else if (section < (num_sections & ~0x1))
-    {
-        return 2;
-    }
-    else
-    {
-        return 1;
-    }
-}
+    /// Design a filter that requires a single second-order section, such as
+    /// a peaking EQ or high shelf.
+    ///
+    /// \param  method  The name of the design method to use, such as "peq_cs".
+    /// \param  section  The index of the section to design.
+    /// \param  frequency  The center/cutoff frequency of the filter, in Hz.
+    /// \param  q  The Q factor of the filter.
+    /// \param  gain_db  The gain of the filter, in dB.
+    /// \param  sample_rate  The sample rate of the system, in Hz.
+    void design_band(const std::string &method, int section, float frequency,
+                     float q, float gain_db, float sample_rate);
 
 
-/// The alignment required for the `state` and `coeffs` parameters passed to
-/// `iir_process()`, for filters that have 4 or more sections.
+    /// Design a filter that requires multiple second-order sections, such as
+    /// a Linkwitz-Riley LPF or HPF.
+    ///
+    /// \param  method  The name of the design method to use, such as
+    ///     "butterworth".
+    /// \param  start_section  The index of the first section allocated to this
+    ///     filter.
+    /// \param  frequency  The center/cutoff frequency of the filter, in Hz.
+    /// \param  order  The order of the filter.
+    /// \param  sample_rate  The sample rate of the system, in Hz.
+    /// \param  max_sections  The maximum number of sections that can be
+    ///    allocated to this filter.
+    void design(const std::string &method, int start_section, float frequency,
+                int order, float sample_rate, int max_sections);
 
-const int IIR_ALIGN = 4 * sizeof(float);
+
+    /// Set the coefficients of a second-order section.  The coefficients are
+    /// internally normalized so that a0 and b0 are always 1.0, and the gain
+    /// of the section is stored separately (to be combined with the gain of
+    /// the other sections).
+    ///
+    /// \param  section  The index of the section to set.
+    /// \param  b0  The numerator coefficient of the z^0 term.
+    /// \param  b1  The numerator coefficient of the z^-1 term.
+    /// \param  b2  The numerator coefficient of the z^-2 term.
+    /// \param  a0  The denominator coefficient of the z^0 term.
+    /// \param  a1  The denominator coefficient of the z^-1 term.
+    /// \param  a2  The denominator coefficient of the z^-2 term.
+    void set_section_coeffs(int section, double b0, double b1, double b2,
+                            double a0, double a1, double a2);
 
 
-/// For multichannel filters, return the size of the `state` memory allocated
-/// for each channel, such that alignment can be guaranteed for all channels.
-/// This allows the state memory for all of the channels to be allocated from
-/// a single aligned array of size `num_channels * iir_state_size(num_sections)`.
-///
-/// \param num_sections  The number of second-order sections in the filter.
-/// \return  `2 * num_sections`, rounded up to a multiple of 4.
+private:
+    int num_sections;
+    int num_channels;
+    int state_size;
+    std::unique_ptr<float[]> coeff;
+    std::unique_ptr<float[]> state;
+    std::vector<float> section_gain;
+    float total_gain;
 
-int iir_state_size(int num_sections);
-
-
-/// Process one frame of audio through an IIR filter.
-///
-/// The coefficients for all sections are normalized, with a1 and a2 being
-/// divided by a0, and b1 and b2 divided by b0.  The gain `g` passed to this
-/// function is the product of b0/a0 of all sections.
-///
-/// This function is optimized to use vector operations, and thus requires the
-/// coefficients in `coeffs` to be stored in a particular order.
-///
-/// For each group of 4 sections, the coefficients are arranged as:
-///
-/// a1_1 a1_2 a1_3 a1_4 a2_1 a2_2 a2_3 a2_4 b1_1 b1_2 b1_3 b1_4 b2_1 b2_2 b2_3 b2_4
-///
-/// If `numSections % 4` is 2 or 3, the groups of 4 sections will be followed
-/// by a group of 2 sections' coefficients arranged as:
-///
-/// a1_1 a1_2 a2_1 a2_2 b1_1 b1_2 b2_1 b2_2
-///
-/// Finally, if `numSections` is odd, these will be followed by the odd
-/// section's coefficients:
-///
-/// a1 a2 b1 b2
-///
-/// Writing coefficients in the correct order can be facilitated using
-/// `coeff_index_start()`, `coeff_index_stride()`, and `A1_INDEX`, etc.  For
-/// example, to write the 4 coefficients to section `n`:
-///
-/// ~~~
-/// start = coeff_index_start(n, num_sections);
-/// stride = coeff_index_stride(n, num_sections);
-/// coeffs[start + A1_INDEX * stride] = a1 / a0;
-/// coeffs[start + A2_INDEX * stride] = a2 / a0;
-/// coeffs[start + B1_INDEX * stride] = b1 / b0;
-/// coeffs[start + B2_INDEX * stride] = b2 / b0;
-/// g *= b0/a0;
-/// ~~~
-///
-/// \param out  The array of output samples, of length `frame_size`.
-/// \param in  The array of input samples, of length `frame_size`.
-/// \param state  The filter's memory, of length 2 * `num_sections`.  This must
-///    be unique for each signal to be filtered, and must be preserved between
-///    calls.  This array must be aligned by `IIR_ALIGN` for filters of 4 or
-///    more sections.
-/// \param g  The overall gain of the filter (product of b0/a0 for each
-///    section).
-/// \param coeffs  The filter coefficients, of length 4 * `num_sections`.  This
-///    array must be aligned by `IIR_ALIGN` for filters of 4 or more sections.
-/// \param num_sections  The number of second-order sections in the filter.
-/// \param frame_size  The number of samples to be processed.
-
-void iir_process(float *out, const float *in, float * state, float g,
-                 const float * coeffs, int num_sections,
-                 int frame_size);
+    void process_impl(float *out, const float *in, int channel, int frame_size);
+};
 
 
 } // namespace filter
