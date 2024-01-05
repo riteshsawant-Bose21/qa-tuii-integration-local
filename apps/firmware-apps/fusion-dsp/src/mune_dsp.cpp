a840966f5fe1698bd3eaa20b9152ea4ebee6b1a9
Michael Pocino <michael_pocino@bose.com>
2024-01-05 17:48:31 -0500

    [MUNE-248] Add profiling to DSP framework
    Added profiling code to the DSP framework.  By default, each task
    is profiled, but the individual blocks within the task are not.
    Profiling each block within a task can be enabled by setting the
    "profile_blocks" constant for that task to true.
    
    The maximum time for each block may not be accurate because such
    short time periods are being measured.  For best results, use a
    configuration with many instances of the same type of block, and
    use the results from the whole task.
    
    There are a few restrictions and limitations with the current
    implementation:
    
    - The profiling results are only printed when the task is
        destroyed.  Running for a limited time (using the -t option)
        will cause the tasks to be destroyed after the given number
        of seconds.
    - The results are only printed when using the JACK thread
        interface, rather than the callback interface.  Run mune_dsp
        with the -T option to enable the thread interface.

--- apps/firmware-apps/fusion-dsp/src/mune_dsp.cpp
+++ apps/firmware-apps/fusion-dsp/src/mune_dsp.cpp
@@ -3,12 +3,14 @@
 
 #include <bosepro/configuration.h>
 #include <bosepro/parameters.h>
+#include <bosepro/profile.h>
 #include <bosepro/session.h>
 
 #include <boost/program_options.hpp>
 #include <jack/jack.h>
 #include <spdlog/spdlog.h>
 
+#include <pthread.h>
 #include <unistd.h>
 
 #include <iostream>
@@ -18,8 +20,72 @@ int rt_process(jack_nframes_t /* unused */, void *arg)
 {
     bosepro::Session *session = (bosepro::Session *)arg;
 
+    if (session == nullptr)
+    {
+        return 0;
+    }
+
+    if (session->finished_running())
+    {
+        SPDLOG_INFO("Finished running.");
+        delete session;
+        SPDLOG_INFO("Session terminated.");
+        return 0;
+    }
+
     session->process();
     return 0;
+}
+
+typedef struct {
+    jack_client_t *client;
+    bosepro::Session *session;
+} ThreadData;
+
+void *rt_thread(void *arg)
+{
+    ThreadData *thread_data = (ThreadData *)arg;
+    sched_param param;
+    int policy;
+    SPDLOG_INFO("Starting thread.");
+    pthread_getschedparam(pthread_self(), &policy, &param);
+    SPDLOG_INFO("Thread priority: {} {} {}", SCHED_FIFO, policy, param.sched_priority);
+
+
+#ifndef USE_MAC_THREADS
+    cpu_set_t cpuset;
+
+    pthread_getaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+    SPDLOG_INFO("CPU affinity: {} {} {} {} {}", CPU_COUNT(&cpuset),
+            CPU_ISSET(0, &cpuset), CPU_ISSET(1, &cpuset), CPU_ISSET(2, &cpuset), CPU_ISSET(3, &cpuset));
+
+    CPU_ZERO(&cpuset);
+    CPU_SET(1, &cpuset);
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) != 0)
+    {
+        SPDLOG_ERROR("pthread_setaffinity_np() failed");
+    }
+
+    pthread_getaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+    SPDLOG_INFO("CPU affinity: {} {} {} {} {}", CPU_COUNT(&cpuset),
+            CPU_ISSET(0, &cpuset), CPU_ISSET(1, &cpuset), CPU_ISSET(2, &cpuset), CPU_ISSET(3, &cpuset));
+#endif
+
+    while (!thread_data->session->finished_running())
+    {
+        jack_cycle_wait(thread_data->client);
+        thread_data->session->process();
+        jack_cycle_signal(thread_data->client, 0);
+    }
+
+    jack_cycle_wait(thread_data->client);
+    SPDLOG_INFO("Finished running.");
+    delete thread_data->session;
+    SPDLOG_INFO("Session terminated.");
+    jack_cycle_signal(thread_data->client, 1);
+
+    SPDLOG_INFO("exiting thread.");
+    return arg;
 }
 
 
@@ -32,6 +98,7 @@ int main(int argc, char *argv[])
     desc.add_options()
         ("configuration,c", boost::program_options::value<std::string>()->default_value("config/configuration.json"), "configuration file")
         ("parameters,p", boost::program_options::value<std::string>()->default_value("config/parameters.json"), "parameter definition file")
+        ("threads,T", "use JACK threads interface")
         ("time,t", boost::program_options::value<int>(), "time to run (seconds)")
         ("help,h", "print this message and exit")
     ;
@@ -47,6 +114,9 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    SPDLOG_INFO("Profile resolution {} ns", bosepro::Profile::get_resolution());
+    bosepro::Profile::set_cpu_mips(1800.0);
+
     bosepro::Configuration configuration(vm["configuration"].as<std::string>());
     bosepro::Parameters parameters(vm["parameters"].as<std::string>());
     bosepro::Session session(configuration.get_session(), parameters);
@@ -61,7 +131,26 @@ int main(int argc, char *argv[])
 
     if (bosepro::Jack::has_client())
     {
-        if (!bosepro::Jack::set_process_callback(rt_process, &session, false))
+        if (vm.count("time"))
+        {
+            session.set_seconds_to_run(vm["time"].as<int>());
+        }
+
+        if (vm.count("threads"))
+        {
+            SPDLOG_INFO("Using JACK threads interface");
+
+            static ThreadData thread_data;
+            thread_data.client = bosepro::Jack::get_client();
+            thread_data.session = &session;
+
+            if (!bosepro::Jack::set_process_thread(rt_thread, &thread_data, false))
+            {
+                SPDLOG_CRITICAL("bosepro::Jack::set_process_thread() failed");
+                return 1;
+            }
+        }
+        else if (!bosepro::Jack::set_process_callback(rt_process, &session, false))
         {
             SPDLOG_CRITICAL("bosepro::Jack::set_process_callback() failed");
             return 1;
