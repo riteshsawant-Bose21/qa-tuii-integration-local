fb42e4b5bd6ff89dbce1d98ebac856b90c7e5e00
Michael Pocino <michael.pocino@boseprofessional.com>
2024-02-27 13:13:22 -0500

    [MUNE-89] Add gate algorithm
    Added a gate algorithm based on the ControlSpace gate.

---
+++ apps/firmware-apps/fusion-dsp/src/gate.cpp
@@ -0,0 +1,151 @@
+
+#include <bosepro/algorithm.h>
+
+#include <cstdint>
+#include <vector>
+
+
+namespace {
+
+
+class Gate : public bosepro::Algorithm
+{
+public:
+    Gate(const bosepro::BlockConfiguration &configuration);
+    virtual ~Gate() = default;
+
+    virtual void process() override;
+
+private:
+    int_fast32_t channels;
+    std::vector<const float *> in;
+    std::vector<float *> out;
+    std::vector<const float *> sidechain_in;
+    bool sidechain_enabled;
+    float threshold;
+    float depth;
+    float attack_time;
+    float attack_coeff;
+    float decay_time;
+    float decay_coeff;
+    float hold_time;
+    int_fast32_t hold_count;
+    int_fast32_t hold_counter;
+    float level_attack_coeff;
+    float level_release_coeff;
+    float current_gain;
+    bool is_open;
+    float smoothed_level;
+
+    void update_hold();
+    void update_attack();
+    void update_decay();
+
+    ALGORITHM_DECLARE(Gate);
+};
+
+ALGORITHM_REGISTER(Gate, "gate");
+
+
+Gate::Gate(const bosepro::BlockConfiguration &configuration)
+    : bosepro::Algorithm(configuration)
+{
+    get_constant("channels", channels);
+
+    assign_terminal("in", &in);
+    assign_terminal("out", &out);
+    assign_terminal("sidechain_in", &sidechain_in);
+
+    assign_control("sidechain_enable", &sidechain_enabled);
+    assign_control("threshold", &threshold);
+    assign_control("depth", &depth);
+    assign_control("attack", &attack_time,
+                   POST_FUNCTION_SCALAR(update_attack));
+    assign_control("hold", &hold_time,
+                   POST_FUNCTION_SCALAR(update_hold));
+    assign_control("decay", &decay_time,
+                   POST_FUNCTION_SCALAR(update_decay));
+
+    assign_meter("gain_meter", &current_gain);
+    assign_meter("open_meter", &is_open);
+
+    level_attack_coeff = 0.341f;
+    current_gain = 1.0f;
+}
+
+
+void Gate::process()
+{
+    float g = current_gain;
+
+    for (int_fast32_t sample = 0; sample < get_frame_size(); sample++)
+    {
+        float level = 0.0f;
+
+        if (!sidechain_enabled)
+        {
+            for (int_fast32_t channel = 0; channel < channels; channel++)
+            {
+                level = std::max(level, std::fabs(in[channel][sample]));
+            }
+        }
+        else
+        {
+            level = std::fabs(sidechain_in[0][sample]);
+        }
+
+        smoothed_level += (level - smoothed_level) *
+            ((level > smoothed_level) ? level_attack_coeff
+                                      : level_release_coeff);
+
+        is_open = smoothed_level >= threshold;
+
+        if (is_open)
+        {
+            hold_counter = hold_count;
+        }
+        else
+        {
+            hold_counter--;
+        }
+
+        if (is_open || hold_counter > 0)
+        {
+            g += (1.0 - g) * attack_coeff;
+        }
+        else
+        {
+            g += (depth - g) * decay_coeff;
+        }
+
+        for (int_fast32_t channel = 0; channel < channels; channel++)
+        {
+            out[channel][sample] = in[channel][sample] * g;
+        }
+    }
+
+    current_gain = g;
+}
+
+
+void Gate::update_hold()
+{
+    hold_count = hold_time * get_sample_rate() / 1000.0f;
+}
+
+
+void Gate::update_attack()
+{
+    attack_coeff = 1.0f - std::expf(-1.0f / (attack_time / 1000.0f *
+                                    get_sample_rate()));
+}
+
+
+void Gate::update_decay()
+{
+    decay_coeff = 1.0f - std::expf(-1.0f / (decay_time / 1000.0f *
+                                   get_sample_rate()));
+    level_release_coeff = decay_coeff / 0.75f;
+}
+
+} // namespace
