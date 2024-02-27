84683e523c371e0d735bb3af9b3372d0a7ae8ffd
Michael Pocino <michael.pocino@boseprofessional.com>
2024-02-27 13:20:38 -0500

    [MUNE-90] Add compressor algorithm
    Added a compressor/limiter algorithm, based on the ControlSpace
    algorithm.

---
+++ apps/firmware-apps/fusion-dsp/src/compressor.cpp
@@ -0,0 +1,141 @@
+
+#include <bosepro/algorithm.h>
+
+#include <cstdint>
+#include <vector>
+
+
+namespace {
+
+
+class Compressor : public bosepro::Algorithm
+{
+public:
+    Compressor(const bosepro::BlockConfiguration &configuration);
+    virtual ~Compressor() = default;
+
+    virtual void process() override;
+
+private:
+    int_fast32_t channels;
+    std::vector<const float *> in;
+    std::vector<float *> out;
+    std::vector<const float *> sidechain_in;
+    bool sidechain_enabled;
+    float threshold;
+    float ratio;
+    float ratio_scale;
+    float attack_time;
+    float attack_coeff;
+    float release_time;
+    float release_coeff;
+    float level_attack_coeff;
+    float level_release_coeff;
+    float current_gain;
+    float smoothed_level;
+
+    void update_ratio();
+    void update_attack();
+    void update_release();
+
+    ALGORITHM_DECLARE(Compressor);
+};
+
+ALGORITHM_REGISTER(Compressor, "compressor");
+
+
+Compressor::Compressor(const bosepro::BlockConfiguration &configuration)
+    : bosepro::Algorithm(configuration)
+{
+    get_constant("channels", channels);
+
+    assign_terminal("in", &in);
+    assign_terminal("out", &out);
+    assign_terminal("sidechain_in", &sidechain_in);
+
+    assign_control("sidechain_enable", &sidechain_enabled);
+    assign_control("threshold", &threshold);
+    assign_control("ratio", &ratio,
+                   POST_FUNCTION_SCALAR(update_ratio));
+    assign_control("attack", &attack_time,
+                   POST_FUNCTION_SCALAR(update_attack));
+    assign_control("release", &release_time,
+                   POST_FUNCTION_SCALAR(update_release));
+
+    assign_meter("gain_meter", &current_gain);
+
+    level_attack_coeff = 0.41666667f;
+    current_gain = 1.0f;
+}
+
+
+void Compressor::process()
+{
+    float g = current_gain;
+
+    for (int_fast32_t sample = 0; sample < get_frame_size(); sample++)
+    {
+        float level = 0.0f;
+
+        if (!sidechain_enabled)
+        {
+            for (int_fast32_t channel = 0; channel < channels; channel++)
+            {
+                level = std::max(level, std::fabs(in[channel][sample]));
+            }
+        }
+        else
+        {
+            level = std::fabs(sidechain_in[0][sample]);
+        }
+
+        smoothed_level += (level - smoothed_level) *
+            ((level > smoothed_level) ? level_attack_coeff
+                                      : level_release_coeff);
+
+        float attenuation = threshold * 0.05f - std::log10f(level);
+        attenuation = std::min(attenuation, 0.0f);
+        attenuation *= ratio_scale;
+        attenuation = std::powf(10.0f, attenuation);
+
+        if (attenuation < g)
+        {
+            g += (attenuation - g) * attack_coeff;
+        }
+        else
+        {
+            g += (1.0 - g) * release_coeff;
+        }
+
+
+        for (int_fast32_t channel = 0; channel < channels; channel++)
+        {
+            out[channel][sample] = in[channel][sample] * g;
+        }
+    }
+
+    current_gain = g;
+}
+
+
+void Compressor::update_ratio()
+{
+    ratio_scale = 1.0f - 1.0f / ratio;
+}
+
+
+void Compressor::update_attack()
+{
+    attack_coeff = 1.0f - std::expf(-1.0f / (attack_time / 1000.0f *
+                                    get_sample_rate()));
+}
+
+
+void Compressor::update_release()
+{
+    release_coeff = 1.0f - std::expf(-1.0f / (release_time / 1000.0f *
+                                     get_sample_rate()));
+    level_release_coeff = release_coeff / 0.75f;
+}
+
+} // namespace
