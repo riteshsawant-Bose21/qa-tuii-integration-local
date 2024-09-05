7d7ba4b2155c0824a27afd68a8f2bbb3dc71695f
Michael Pocino <michael.pocino@boseprofessional.com>
2024-09-05 15:36:34 -0400

    [FUSION-112] Add ducker algorithm
    The ducker algorithm was ported from CSD.  It uses a single
    sidechain input, which ducks the primary channels whenver the
    sidchain signal is above a threshold.  The parameters and their
    defaults are defined as in the PO Processing Objects spreadsheet.

---
+++ apps/firmware-apps/fusion-dsp/src/ducker.cpp
@@ -0,0 +1,136 @@
+
+#include <bosepro/algorithm.h>
+
+#include <cmath>
+#include <cstdint>
+
+
+namespace {
+
+
+class Ducker : public bosepro::Algorithm
+{
+public:
+    Ducker(const bosepro::BlockConfiguration &configuration);
+    virtual ~Ducker() = default;
+
+    virtual void process() override;
+
+private:
+    int_fast32_t channels;
+    bosepro::DspSignalMemory<const float *[]> in;
+    bosepro::DspSignalMemory<float *[]> out;
+    bosepro::DspSignalMemory<const float *[]> sidechain_in;
+    float threshold;
+    float range;
+    float attack_time;
+    float attack_coeff;
+    float decay_time;
+    float decay_coeff;
+    float hold_time;
+    int_fast32_t hold_count;
+    int_fast32_t hold_counter;
+    float level_attack_coeff;
+    float level_release_coeff;
+    float current_gain;
+    bool is_active;
+    float smoothed_level;
+
+    void update_hold();
+    void update_attack();
+    void update_decay();
+
+    ALGORITHM_DECLARE(Ducker);
+};
+
+ALGORITHM_REGISTER(Ducker, "ducker");
+
+
+Ducker::Ducker(const bosepro::BlockConfiguration &configuration)
+    : bosepro::Algorithm(configuration)
+{
+    get_constant("channels", channels);
+
+    assign_terminal("in", in);
+    assign_terminal("out", out);
+    assign_terminal("sidechain_in", sidechain_in);
+
+    assign_control("threshold", &threshold);
+    assign_control("range", &range);
+    assign_control("attack", &attack_time,
+                   POST_FUNCTION_SCALAR(update_attack));
+    assign_control("hold", &hold_time,
+                   POST_FUNCTION_SCALAR(update_hold));
+    assign_control("decay", &decay_time,
+                   POST_FUNCTION_SCALAR(update_decay));
+
+    assign_meter("gain_meter", &current_gain);
+
+    level_attack_coeff = 0.341f;
+    current_gain = 1.0f;
+}
+
+
+void Ducker::process()
+{
+    float g = current_gain;
+
+    for (int_fast32_t sample = 0; sample < get_frame_size(); sample++)
+    {
+        float level = std::fabs(sidechain_in[0][sample]);
+
+        smoothed_level += (level - smoothed_level) *
+            ((level > smoothed_level) ? level_attack_coeff
+                                      : level_release_coeff);
+
+        is_active = smoothed_level >= threshold;
+
+        if (is_active)
+        {
+            hold_counter = hold_count;
+        }
+        else
+        {
+            hold_counter--;
+        }
+
+        if (is_active || hold_counter > 0)
+        {
+            g += (range - g) * attack_coeff;
+        }
+        else
+        {
+            g += (1.0f - g) * decay_coeff;
+        }
+
+        for (int_fast32_t channel = 0; channel < channels; channel++)
+        {
+            out[channel][sample] = in[channel][sample] * g;
+        }
+    }
+
+    current_gain = g;
+}
+
+
+void Ducker::update_hold()
+{
+    hold_count = hold_time * get_sample_rate() / 1000.0f;
+}
+
+
+void Ducker::update_attack()
+{
+    attack_coeff = 1.0f - expf(-1.0f / (attack_time / 1000.0f *
+                               get_sample_rate()));
+}
+
+
+void Ducker::update_decay()
+{
+    decay_coeff = 1.0f - expf(-1.0f / (decay_time / 1000.0f *
+                              get_sample_rate()));
+    level_release_coeff = decay_coeff / 0.75f;
+}
+
+} // namespace
