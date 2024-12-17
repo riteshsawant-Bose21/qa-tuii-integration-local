6475a6739e295eb9e63e917d88a679fd92848238
Amol-Kirtane-Boseprofessional <153024044+Amol-Kirtane-Boseprofessional@users.noreply.github.com>
2024-12-17 21:09:04 +0530

    uncomment cleanup stage

--- apps/firmware-apps/fusion-server/Jenkins/Jenkinsfile.groovy
+++ apps/firmware-apps/fusion-server/Jenkins/Jenkinsfile.groovy
@@ -3,7 +3,7 @@
 @Library(['ProJenkinsSharedLibrary']) _
 import JenkinsJobCleanup
 def JobCleanup = new JenkinsJobCleanup()
-// JobCleanup.CheckPendingJob("Fusion/Fusion-Server/Build-Fusion-Server", env.BRANCH_NAME)
+JobCleanup.CheckPendingJob("Fusion/Fusion-Server/Build-Fusion-Server", env.BRANCH_NAME)
 def VERSION = ''
 def ARTIFACTORY_TARGET = "pro-fusion-local/Fusion-Server/%s/%s/%s/%s"
 
@@ -128,10 +128,10 @@ pipeline {
 			  }
 			}
         }
-// 	cleanup {
-// 			retry(3){
-// 			    cleanWs()
-// 			}
-// 		}
+ 	cleanup {
+ 			retry(3){
+ 			    cleanWs()
+ 			}
+ 		}
 	}
 }
