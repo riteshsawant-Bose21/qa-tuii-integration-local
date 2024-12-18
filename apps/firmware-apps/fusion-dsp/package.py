6037bfc0815bcfe1193dd4b8983ecdd218e7f843
Matt Myers <56367938+matthew-myers-boseprofessional@users.noreply.github.com>
2024-12-18 09:44:20 -0700

    [DEVOPS-61] Create a build package (#42)
    * Added a python script to create a build package
    * Added an empty base configuration.
    * Updated the prototype1_demo config to have a frame size of 32.

---
+++ apps/firmware-apps/fusion-dsp/package.py
@@ -0,0 +1,55 @@
+# Make a tar.gz package of a build.
+# Usage: python3 package.py
+
+import tarfile
+import subprocess
+import datetime
+import os
+
+
+APP_PATH = 'build/mune_dsp'
+INFO_PATH = 'build/fusion-dsp.info'
+
+# TODO: Get a real version for mune_dsp.
+version = "0.0.1"
+
+git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('ascii').strip()
+commit_ts = int(subprocess.check_output(['git', 'show', '-s', '--format=%ct', 'HEAD']).decode('ascii').strip())
+build_ts = os.path.getmtime(APP_PATH)
+
+# Get the commit date and build date in UTC.
+git_date = datetime.datetime.fromtimestamp(commit_ts, tz=datetime.timezone.utc).isoformat() 
+build_date = datetime.datetime.fromtimestamp(build_ts, tz=datetime.timezone.utc).isoformat('T', 'seconds')
+version += f'+{git_hash[0:7]}'
+
+# Write the fusion-dsp.info file.
+with open(INFO_PATH, "w") as info_file:
+    info_file.write(f'APP="mune-dsp"\n')
+    info_file.write(f'VERSION="{version}"\n')
+    info_file.write(f'BRANCH="{git_branch}"\n')
+    info_file.write(f'COMMIT_ID="{git_hash}"\n')
+    info_file.write(f'COMMIT_DATE="{git_date}"\n')
+    info_file.write(f'BUILD_DATE="{build_date}"\n')
+
+# Add the files needed to install in the target image.
+required_files = [
+    INFO_PATH,
+    APP_PATH,
+    'config/algorithm-definitions.json',
+    'config/base.json',
+    'config/prototype1_demo.json'
+]
+
+print(f'Creating build package...')
+
+TAR_PATH = f'build/fusion-dsp_{version}.tar.gz'
+with tarfile.open(TAR_PATH, 'w:gz') as tar:
+    for f in required_files:
+        print(f'  add {f}')
+        tar.add(f)
+
+# Clean up temporary files.
+os.remove(INFO_PATH)
+
+print(f'Created: {TAR_PATH}')
