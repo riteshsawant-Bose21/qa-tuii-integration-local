d4aa3b4b9cfbb4411a887c4983525ccec3eaba07
Amol-Kirtane-Boseprofessional <153024044+Amol-Kirtane-Boseprofessional@users.noreply.github.com>
2025-03-26 21:28:09 -0400

    Update Jenkins script to push artifacts to nexus repository

--- apps/firmware-apps/fusion-server/Jenkins/Jenkinsfile.groovy
+++ apps/firmware-apps/fusion-server/Jenkins/Jenkinsfile.groovy
@@ -4,7 +4,7 @@
 import JenkinsJobCleanup
 def JobCleanup = new JenkinsJobCleanup()
 def VERSION = ''
-def ARTIFACTORY_TARGET = "pro-fusion-local/Fusion-Server/%s/%s/%s/%s"
+def NEXUS_TARGET = "Fusion-Server/%s/%s"
 
 pipeline {
 	agent { label 'pro-win' }
@@ -12,6 +12,8 @@ pipeline {
 		timeout(time: 2, unit: 'HOURS')
 	}
 	environment {
+		NEXUS_URL = '10.100.109.38:8081'
+		CREDENTIALS_ID = 'jenkins-artifact-deployer'
 		buildNumber="${env.BUILD_NUMBER}"
 		buildDir="${env.WORKSPACE}"
 		BuildType="Continuous"
@@ -78,28 +80,27 @@ pipeline {
 					}
 				}
 				stage('Deploy') {
-					environment {
-						ASSETS_CREDS = credentials('pro-jenkins-artifactory')
-					}
 					steps {
-    					script {
-                            				EMBEDDED_PATH_PART=env.CHANGE_BRANCH ?: env.BRANCH_NAME
-                            				def server = Artifactory.server 'Bose-Artifactory'
-                            				def fileName = "fusion-server_*.tar.gz"
-                            				def binaryPath = sh(returnStdout: true, script: "find . -name '${fileName}' | head -1").trim()
-							def binaryFileName = sh(returnStdout: true, script: "basename -- '${binaryPath}'").trim()
-							def targetPath = String.format(ARTIFACTORY_TARGET, env.BuildType, EMBEDDED_PATH_PART, env.VERSION, binaryFileName)
-							def fileSpec = """{
-								"files": [
-									{
-									"pattern": "${binaryPath}",
-									"flat": "false",
-									"target": "${targetPath}"
-									}
-								]
-							}"""
-							println("fileSpec: ${fileSpec}")
-							server.upload spec:fileSpec
+	    					script {
+	                   				dir( 'build/' ) {
+	                                			EMBEDDED_PATH_PART=env.CHANGE_BRANCH ?: env.BRANCH_NAME
+	    						    	def artifacts = []
+								def filesToUpload = findFiles(glob: 'fusion-server_*.tar.gz')
+								filesToUpload.each { file ->
+	                                    				artifacts.add([artifactId: file.name.substring(0, file.name.lastIndexOf('_')), file: file.path, type: 'tar.gz'])
+								}
+	    						    	def targetPath = String.format(NEXUS_TARGET, env.BuildType, EMBEDDED_PATH_PART)
+								nexusArtifactUploader(
+				                                    nexusVersion: 'nexus3',
+				                                    protocol: 'http',
+				                                    nexusUrl: "${NEXUS_URL}",
+				                                    repository: "pro-fusion-local/${targetPath}",
+				                                    version: "${env.VERSION}",
+				                                    groupId: "",
+				                                    credentialsId: "${CREDENTIALS_ID}",
+				                                    artifacts: artifacts
+				                                )
+	    					    	}
     						}
 					}
 				}
