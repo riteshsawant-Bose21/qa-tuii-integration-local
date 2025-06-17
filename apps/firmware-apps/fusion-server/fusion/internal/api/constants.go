98ed465990baf58db12689566c5f170c5e816674
BoseGene <gene.ragan@boseprofessional.com>
2025-06-17 11:13:41 -0700

    Remove ancient unused ZMQPort (#73)
    * Remove ancient unused ZMQPort
    
    * More unused port contants removed

--- apps/firmware-apps/fusion-server/fusion/internal/api/constants.go
+++ apps/firmware-apps/fusion-server/fusion/internal/api/constants.go
@@ -1,13 +1,10 @@
 package api
 
 const (
-	AdminPort             = "9090"
-	AnalogControllerPort  = "8002"
-	DigitalControllerPort = "8003"
-	HTTPPort              = "8080"
-	SAPPort               = "9875" // As defined: https://datatracker.ietf.org/doc/html/rfc2974
-	UDPPort               = "7947"
-	ZMQPort               = "5678"
+	AdminPort = "9090"
+	HTTPPort  = "8080"
+	SAPPort   = "9875" // As defined: https://datatracker.ietf.org/doc/html/rfc2974
+	UDPPort   = "7947"
 
 	ContentType  = "Content-Type"
 	JsonMIMEType = "application/json"
