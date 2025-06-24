f8befc183f93d0f03a27bdf4d32655906ccbc61a
Amol-Kirtane-Boseprofessional <153024044+Amol-Kirtane-Boseprofessional@users.noreply.github.com>
2025-06-24 17:23:41 -0400

    fix the build folder

--- apps/firmware-apps/fusion-dsp/Jenkins/Jenkinsfile.groovy
+++ apps/firmware-apps/fusion-dsp/Jenkins/Jenkinsfile.groovy
@@ -7,7 +7,7 @@ import groovy.json.JsonOutput
 def VERSION = ''
 def JobCleanup = new JenkinsJobCleanup()
 
-NEXUS_TARGET = "pro-fusion-local/Fusion-DSP/%s/%s"
+NEXUS_TARGET = "pro-fusion-local/Fusion-DSP/%s/%s/fusion-dsp/%s"
 
 pipeline {
 	agent { label 'pro-fusion-variscite-container' }
@@ -127,7 +127,7 @@ pipeline {
 				// 			filesToUpload.each { file ->
     //                                 				artifacts.add([artifactId: file.name.substring(0, file.name.lastIndexOf('_')), file: file.path, type: 'tar.gz'])
 				// 			}
-    						def targetPath = String.format(NEXUS_TARGET, env.BuildType, EMBEDDED_PATH_PART)
+    						def targetPath = String.format(NEXUS_TARGET, env.BuildType, EMBEDDED_PATH_PART, env.VERSION)
 				// 			nexusArtifactUploader(
 			 //                                   nexusVersion: 'nexus3',
 			 //                                   protocol: 'http',
@@ -142,7 +142,7 @@ pipeline {
 	                            filesToUpload.each { file ->
 				                    sh """
 				                        set -e
-				                        curl --ssl-no-revoke -f -u $USERNAME:$PASSWORD -T "${file.path}" "${NEXUS_HTTPS_URL}/${targetPath}/fusion-dsp/${VERSION}/${file.name}"
+				                        curl --ssl-no-revoke -f -u $USERNAME:$PASSWORD -T "${file.path}" "${NEXUS_HTTPS_URL}/${targetPath}/${file.name}"
 				                    """
 									println("Upload succeeded: ${NEXUS_HTTPS_URL}/${targetPath}/${file.name}")
 				                }
