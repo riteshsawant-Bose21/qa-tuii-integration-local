1a89a8454778d1ea99ce66a0739bf49b3c5f2cf1
Sujith Devadas <sujith.devadas@boseprofessional.com>
2025-08-14 12:13:06 +0530

    prepared react-apps folder-structure and integrated moon task runner for linting anf formatting

--- apps/frontend/react-apps/packages/backend/utils/sendResponse.js
+++
@@ -1,9 +0,0 @@
-export const sendResponse = (
-  res,
-  statusCode = null,
-  statusMessage = "",
-  message = "",
-  data = {}
-) => {
-  return res.status(statusCode).json({ status: statusMessage, message, data });
-};
--- apps/frontend/react-apps/packages/customer/src/api/organization.ts
+++
@@ -1,5 +0,0 @@
-import type { Organization } from 'xyte-api';
-import api from './axios';
-
-export const getOrganization = () =>
-    api.get<Organization>('/organization').then(r => r.data);
