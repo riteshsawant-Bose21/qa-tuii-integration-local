d9b703e76a429649ecbd42c64bf74df083648dff
thanin-kongkiatsophon-boseprofessional <thanin.kongkiatsophon@boseprofessional.com>
2025-08-29 11:35:00 -0400

    Jack in polarity and meter

--- apps/firmware-apps/fusion-dsp/src/jack_in.cpp
+++ apps/firmware-apps/fusion-dsp/src/jack_in.cpp
@@ -18,10 +18,9 @@ public:
 private:
     bosepro::DspSignalMemory<float *[]> out;
 
-    bosepro::DspTelemetryMemory<float[]> out_meter;
     bosepro::DspTelemetryMemory<float[]> in_meter;
 
-    bool polarity;
+    bosepro::DspCoeffMemory<bool[]> polarity;
 
     ALGORITHM_DECLARE(JackIn);
 };
@@ -34,10 +33,9 @@ JackIn::JackIn(const bosepro::BlockConfiguration &configuration)
 {
     assign_terminal("out", out);
 
-    assign_telemetry("out_meter", out_meter);
     assign_telemetry("in_meter", in_meter, bosepro::linear_to_db);
 
-    assign_parameter("invert_polarity", &polarity);
+    assign_parameter("invert_polarity", polarity);
 }
 
 
@@ -45,26 +43,23 @@ void JackIn::process()
 {
     for (int channel = 0; channel < channels; channel++)
     {
-        int fs = get_frame_size();
-        float *in = ports[channel].get_buffer(fs);
-
-    if (polarity) {
-            for (int i = 0; i < fs; ++i) {
-                in[i] = -in[i]; 
-            }
-        }
+        float *in = ports[channel].get_buffer(get_frame_size());
 
         in_meter[channel] = 0.0;
 
-        for (int i = 0; i < fs; ++i)
+        for (int i = 0; i < get_frame_size(); ++i)
         {
             float a = std::fabs(in[i]);
             in_meter[channel] = std::max(in_meter[channel], a);
         }
 
-        out_meter[channel] = *in; 
+        std::memcpy(out[channel], in, get_frame_size() * sizeof(float));
 
-        std::memcpy(out[channel], in, fs * sizeof(float));
+        if (polarity[channel]) {
+            for (int i = 0; i < get_frame_size(); ++i) {
+                out[channel][i] = -out[channel][i];
+            }
+        }
     }
 }
 
