37b6a00aad0e6cdd11a4b8d8ebc668e1f07fcb50
Amol-Kirtane-Boseprofessional <153024044+Amol-Kirtane-Boseprofessional@users.noreply.github.com>
2025-09-09 13:42:03 -0400

    CI: Update telemetry-core workflow and overall project deps

--- apps/firmware-apps/fusion-telemetry-core/moon-build.sh
+++ apps/firmware-apps/fusion-telemetry-core/moon-build.sh
@@ -4,4 +4,3 @@ set -euo pipefail
 # Build the fusion-dsp binary in a single moon task.
 
 source /home/ec2-user/fusion-mini-build-cache/bose/fusion/yocto-sdk/environment-setup-cortexa53-crypto-poky-linux
-make telemetry_core
