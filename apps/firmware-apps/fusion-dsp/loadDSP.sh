3c70644f596d5b8e088a03457607f69cafd3488a
Amol-Kirtane-Boseprofessional <153024044+Amol-Kirtane-Boseprofessional@users.noreply.github.com>
2025-10-03 09:49:30 -0400

    Clear earlier version of fusion-dsp project in monorepo

--- apps/firmware-apps/fusion-dsp/loadDSP.sh
+++
@@ -1,5 +0,0 @@
-IP='139.68.100.156'
-
-sshpass -p 'Bose123' scp -O -oHostKeyAlgorithms=+ssh-rsa jack_dep root@${IP}:/home/root
-sshpass -p 'Bose123' scp -O -oHostKeyAlgorithms=+ssh-rsa libfmt.so.10 root@${IP}:/usr/lib
-sshpass -p 'Bose123' scp -O -oHostKeyAlgorithms=+ssh-rsa libboost_program_options.so.1.66.0 root@${IP}:/usr/lib
