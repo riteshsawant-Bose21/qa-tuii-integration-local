0cffcab819f76a7453b58958a6d46e1d8f255dd7
Sharath B Naik <sharath.naik@boseprofessional.com>
2026-01-07 12:42:37 +0530

    feat: Refactor CreateZoneViewModel and update import paths

--- apps/frontend/flutter-apps/fusion-launcher/lib/features/create_zone_popup/create_zone_popup.dart
+++ apps/frontend/flutter-apps/fusion-launcher/lib/features/create_zone_popup/create_zone_popup.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fusion_launcher/core/service_locator.dart';
 import 'package:fusion_launcher/features/authentication/launcher_sign_in_page.dart';
 import 'package:fusion_launcher/features/configuration/presentation/viewmodel/project_view_model.dart';
-import 'package:fusion_launcher/features/create_zone_popup/view_model/create_zone_viewmodel.dart';
+import 'package:fusion_launcher/features/create_zone_popup/view_model/viewmodel.dart';
 import 'package:fusion_launcher/features/projects/widget/building/widgets/drop_down.dart';
 import 'package:fusion_lib/fusion_lib.dart';
 import 'package:fusion_lib/fusion_theme/app_theme.dart';
