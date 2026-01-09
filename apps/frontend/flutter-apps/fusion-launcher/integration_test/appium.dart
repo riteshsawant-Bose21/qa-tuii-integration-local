c4c90281188ca9055e123d04e3bddf62e1c0e4e4
Lavanya-BoseProfessional <lavanya.rao@boseprofessional.com>
2026-01-09 10:38:17 +0530

    Update appium.dart

--- apps/frontend/flutter-apps/fusion-launcher/integration_test/appium.dart
+++ apps/frontend/flutter-apps/fusion-launcher/integration_test/appium.dart
@@ -1,8 +1,11 @@
 import 'package:appium_flutter_server/appium_flutter_server.dart';
+import 'package:desktop_auth0_flutter/desktop_auth0_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fusion_launcher/core/config/app_config.dart';
 import 'package:fusion_launcher/core/service_locator.dart';
 import 'package:fusion_launcher/main.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 void main() {
   initializeTest(
@@ -10,7 +13,22 @@ void main() {
       // Any prerequisite steps or initialise any dependencies required by the app
       // and make sure to pump the app widget at last.
       WidgetsFlutterBinding.ensureInitialized();
-      await setupServiceLocator();
+    
+    await AppConfig.initialize();
+
+    await setupServiceLocator();
+    if (UniversalPlatform.isWindows) {
+      await initDesktopAuth0Flutter(
+        const DesktopAuth0FlutterInitOptions(
+          bundleName: 'com.bosepro.fusion',
+          auth0Scheme: 'com.bosepro.fusion',
+          categories: 'Office;Productivity',
+          comment: 'Fusion Launcher',
+          name: 'Fusion Launcher',
+          iconAssetPath: 'assets/images/splash/splash_app_icon.png',
+        ),
+      );
+    }
       await tester.pumpWidget(const MyApp());
     },
   );
