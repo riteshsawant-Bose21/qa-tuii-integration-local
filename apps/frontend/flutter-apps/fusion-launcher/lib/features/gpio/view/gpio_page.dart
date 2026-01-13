bf38b04048650520bd37c01e6d90cd275d18127f
Rithesh Salyan <rithesh.salyan@boseprofessional.com>
2026-01-13 12:32:19 +0530

    missed files from previous commit

--- apps/frontend/flutter-apps/fusion-launcher/lib/features/gpio/view/gpio_page.dart
+++ apps/frontend/flutter-apps/fusion-launcher/lib/features/gpio/view/gpio_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fusion_launcher/core/service_locator.dart';
 import 'package:fusion_launcher/core/utils/broadcast_controllers.dart';
+import 'package:fusion_launcher/core/widgets/title_text_field_switcher.dart';
 import 'package:fusion_launcher/features/configuration/presentation/viewmodel/gpio/gpio_view_model.dart';
 import 'package:fusion_launcher/features/scheduling/view/scheduling_page.dart';
 import 'package:fusion_lib/fusion_lib.dart';
@@ -168,7 +169,7 @@ class GpioPage extends StatelessWidget {
                             itemBuilder: (BuildContext context, int index) {
                               final GpioConfig gpio = state.gpios[index];
                               return <Widget>[
-                                _TitleTextFieldSwitcher(
+                                TitleTextFieldSwitcher(
                                   value: gpio.name,
                                   style: context.textTheme.bodyMedium!,
                                   save: (String value) {
@@ -343,119 +344,6 @@ class GpioPage extends StatelessWidget {
             );
           },
         ),
-      ),
-    );
-  }
-}
-
-class _TitleTextFieldSwitcher extends StatefulWidget {
-  const _TitleTextFieldSwitcher({
-    super.key,
-    required this.save,
-    required this.style,
-    required this.value,
-  });
-  final String value;
-  final ValueChanged<String> save;
-  final TextStyle style;
-  @override
-  State<_TitleTextFieldSwitcher> createState() => __TitleTextFieldSwitcherState();
-}
-
-class __TitleTextFieldSwitcherState extends State<_TitleTextFieldSwitcher> {
-  final TextEditingController controller = TextEditingController();
-  final FocusNode focusNode = FocusNode();
-  bool isEditing = false;
-  late String currentValue;
-
-  @override
-  void initState() {
-    super.initState();
-    currentValue = widget.value;
-    controller.text = widget.value;
-
-    focusNode.addListener(() {
-      if (!focusNode.hasFocus && isEditing) {
-        _saveValue();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    focusNode.dispose();
-    super.dispose();
-  }
-
-  void _startEditing() {
-    setState(() {
-      isEditing = true;
-      controller.text = currentValue;
-    });
-
-    // Focus and select all text after the widget is built
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      focusNode.requestFocus();
-      controller.selection = TextSelection(
-        baseOffset: 0,
-        extentOffset: controller.text.length,
-      );
-    });
-  }
-
-  void _saveValue() {
-    if (isEditing) {
-      if (controller.text.isEmpty) {
-        _cancelEditing();
-        return;
-      }
-      setState(() {
-        currentValue = controller.text;
-        isEditing = false;
-      });
-      widget.save(controller.text);
-    }
-  }
-
-  void _cancelEditing() {
-    setState(() {
-      isEditing = false;
-      controller.text = currentValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (isEditing) {
-      return Focus(
-        onKeyEvent: (FocusNode node, KeyEvent event) {
-          if (event.logicalKey == LogicalKeyboardKey.escape) {
-            _cancelEditing();
-            return KeyEventResult.handled;
-          }
-          return KeyEventResult.ignored;
-        },
-        child: TextField(
-          controller: controller,
-          focusNode: focusNode,
-          style: widget.style,
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.zero,
-            isDense: true,
-          ),
-          onSubmitted: (_) => _saveValue(),
-          onTapOutside: (_) => _saveValue(),
-        ),
-      );
-    }
-
-    return InkWell(
-      onTap: _startEditing,
-      child: Text(
-        currentValue,
-        style: widget.style,
       ),
     );
   }
