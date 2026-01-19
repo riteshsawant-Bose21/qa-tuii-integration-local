5620d4178f6dad1daa11cea667a77d89a6a1ca71
Sharath B Naik <sharath.naik@boseprofessional.com>
2026-01-19 12:39:16 +0530

    feat: add additional size constants for icons, elevation, and font in FusionSizes class

--- libs/flutter-libs/fusion_lib/lib/fusion_theme/fusion_sizes.dart
+++ libs/flutter-libs/fusion_lib/lib/fusion_theme/fusion_sizes.dart
@@ -12,4 +12,20 @@ class FusionSizes {
   static const double padding12 = 12.0;
   static const double padding16 = 16.0;
   static const double padding24 = 24.0;
+
+  static const double iconSize8 = 8.0;
+  static const double iconSize12 = 12.0;
+  static const double iconSize16 = 16.0;
+  static const double iconSize24 = 24.0;
+
+  static const double elevation1 = 1.0;
+  static const double elevation2 = 2.0;
+  static const double elevation4 = 4.0;
+  static const double elevation8 = 8.0;
+
+  static const double fontSize8 = 8.0;
+  static const double fontSize12 = 12.0;
+  static const double fontSize16 = 16.0;
+  static const double fontSize24 = 24.0;
+  static const double fontSize32 = 32.0;
 }
