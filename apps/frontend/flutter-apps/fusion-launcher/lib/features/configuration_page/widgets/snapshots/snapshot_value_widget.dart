0979f3323b9f57a9861714f23de558f20951f96b
suhas-BoseProfessional <suhas.shetty@boseprofessional.com>
2026-01-20 22:11:04 +0530

    feat: update UI colors and styling for improved visibility and consistency

--- apps/frontend/flutter-apps/fusion-launcher/lib/features/configuration_page/widgets/snapshots/snapshot_value_widget.dart
+++ apps/frontend/flutter-apps/fusion-launcher/lib/features/configuration_page/widgets/snapshots/snapshot_value_widget.dart
@@ -45,7 +45,7 @@ class _SnapshotValueWidgetState extends State<SnapshotValueWidget> {
               Icon(
                 isMute ? Icons.volume_off : Icons.volume_up,
                 size: 20,
-                color: context.colorScheme.primaryBlack,
+                color: context.colorScheme.primaryWhite,
               ),
               const SizedBox(width: 8),
               FusionAppText(
@@ -67,14 +67,14 @@ class _SnapshotValueWidgetState extends State<SnapshotValueWidget> {
                 data: SliderTheme.of(context).copyWith(
                   thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 6),
                   overlayShape: const RoundSliderOverlayShape(overlayRadius: 4),
-                  trackHeight: 1,
-                  thumbColor: context.colorScheme.primaryBlack,
+                  trackHeight: 2,
+                  thumbColor: context.colorScheme.primaryWhite,
                 ),
                 child: Slider(
                   value: currentValue,
                   padding: EdgeInsets.zero,
-                  activeColor: context.colorScheme.primaryBlack,
-                  inactiveColor: context.colorScheme.primaryBlack,
+                  activeColor: context.colorScheme.elevation2,
+                  inactiveColor: context.colorScheme.primaryWhite,
                   min: 0,
                   max: 100,
                   divisions: 100,
@@ -95,7 +95,7 @@ class _SnapshotValueWidgetState extends State<SnapshotValueWidget> {
               alignment: Alignment.center,
               padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
               decoration: BoxDecoration(
-                color: context.colorScheme.primaryBlack,
+                color: context.colorScheme.elevation1,
                 borderRadius: BorderRadius.circular(2),
               ),
               child: FusionAppText(text: currentValue.toStringAsFixed(0), style: Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 9)),
@@ -253,10 +253,10 @@ class _SnapshotValueWidgetState extends State<SnapshotValueWidget> {
                     // enabled: isEnabled,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(borderSide: BorderSide(color: context.colorScheme.primaryBlack)),
+                      border: OutlineInputBorder(borderSide: BorderSide(color: context.colorScheme.elevation1)),
                       isDense: true,
                       labelText: "Duration (ms)",
-                      labelStyle: const TextStyle(fontSize: 10),
+                      labelStyle: context.textTheme.bodySmall,
                       contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
                     ),
                     onChanged: (String v) {
