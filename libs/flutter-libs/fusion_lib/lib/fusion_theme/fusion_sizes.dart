6d2adb04f074bfa6f4cc6b7ef69fb84c0bee74bb
suhas-BoseProfessional <suhas.shetty@boseprofessional.com>
2026-01-21 16:19:24 +0530

    feat: enhance expandable zone widget UI with improved padding and margin for better layout

--- libs/flutter-libs/fusion_lib/lib/fusion_theme/fusion_sizes.dart
+++ libs/flutter-libs/fusion_lib/lib/fusion_theme/fusion_sizes.dart
@@ -1,4 +1,5 @@
 class FusionSizes {
+  static const double borderRadius4 = 4.0;
   static const double borderRadius8 = 8.0;
   static const double borderRadius12 = 12.0;
   static const double borderRadius16 = 16.0;
