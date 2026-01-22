f1de5387840dc8d98494682e9d10f2b745a7c14c
Nathan Mark <nathan.mark@boseprofessional.com>
2026-01-22 14:40:54 -0800

    disabling tcal6408 irq

--- apps/kernel-modules/fusion-io-lkm/fusion_io_config.c
+++ apps/kernel-modules/fusion-io-lkm/fusion_io_config.c
@@ -172,7 +172,7 @@ const struct base_device bd_fusion_fm6 = {
         .sn = "tbd",
         .type = BD_TYPE_FUSION_FM6
     },
-    .num_gpios = 5,
+    .num_gpios = 4,
     .gpios = (struct endpoint_gpio[]) {
         {
             .name = "gpio_a_mute_out",
@@ -180,13 +180,13 @@ const struct base_device bd_fusion_fm6 = {
             .num = 5, // GPIO1_IO5
             .dir = EP_GPIO_DIR_I
         },
-        {
-            .name = "gpio_tca9544_int",
-            .type = EP_GPIO_TYPE_PHYS,
-            .is_irq = true,
-            .num = 6, // GPIO1_IO6
-            .trigger_type = IRQ_TYPE_LEVEL_LOW
-        },
+        // {
+        //     .name = "gpio_tca9544_int",
+        //     .type = EP_GPIO_TYPE_PHYS,
+        //     .is_irq = true,
+        //     .num = 6, // GPIO1_IO6
+        //     .trigger_type = IRQ_TYPE_LEVEL_LOW
+        // },
         {
             .name = "gpio_ui_rstn",
             .type = EP_GPIO_TYPE_PHYS,
@@ -199,13 +199,6 @@ const struct base_device bd_fusion_fm6 = {
             .type = EP_GPIO_TYPE_PHYS,
             .export = true,
             .num = 8 // GPIO1_IO8
-        },
-        {
-            .name = "gpio_tcal6408_int",
-            .type = EP_GPIO_TYPE_PHYS,
-            .is_irq = true,
-            .num = 14, // GPIO1_IO14
-            .trigger_type = IRQ_TYPE_LEVEL_LOW
         },
         {
             .name = "gpio_uv_warn",
@@ -1078,7 +1071,7 @@ const struct base_device bd_fusion_fm8y = {
         .sn = "tbd",
         .type = BD_TYPE_FUSION_FM8Y
     },
-    .num_gpios = 5,
+    .num_gpios = 4,
     .gpios = (struct endpoint_gpio[]) {
         {
             .name = "gpio_a_mute_out",
@@ -1105,13 +1098,6 @@ const struct base_device bd_fusion_fm8y = {
             .type = EP_GPIO_TYPE_PHYS,
             .export = true,
             .num = 8 // GPIO1_IO8
-        },
-        {
-            .name = "gpio_tcal6408_int",
-            .type = EP_GPIO_TYPE_PHYS,
-            .is_irq = true,
-            .num = 14, // GPIO1_IO14
-            .trigger_type = IRQ_TYPE_LEVEL_LOW
         },
         {
             .name = "gpio_uv_warn",
