25ad1a17ec823760e5919b86bc82d1be546a69df
Sharath B Naik <sharath.naik@boseprofessional.com>
2026-02-12 15:58:40 +0530

    Refactor AGC Block: Remove PEQ Band Section, Graph, and Output Meter
    - Deleted unused files: _peq_band_section.dart, _peq_graph.dart, and _peq_out_meter.dart.
    - Updated agc_block.dart to remove references to the deleted files and reorganized the layout for better readability.
    - Adjusted UI components to maintain functionality after the removal of PEQ-related sections.

--- apps/frontend/flutter-apps/fusion-launcher/lib/features/processing_block/view/processing_blocks/agc/agc_block.dart
+++ apps/frontend/flutter-apps/fusion-launcher/lib/features/processing_block/view/processing_blocks/agc/agc_block.dart
@@ -7,16 +7,14 @@ import 'package:fusion_launcher/features/processing_block/view/widgets/pb_meter.
 import 'package:fusion_launcher/features/processing_block/view/widgets/widgets.dart';
 import 'package:fusion_launcher/features/zone_functions/widgets/neumorphic_gain_text_field.dart';
 import 'package:fusion_lib/fusion_lib.dart';
-import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:provider/provider.dart';
 
 import '../../../viewmodel/algorithm_data_viewmodel.dart';
 import '../widgets/disabled_widget_wrapper.dart';
 
-part '_peq_band_section.dart';
-part '_peq_controller.dart';
-part '_peq_graph.dart';
-part '_peq_out_meter.dart';
+part '_agc_controller.dart';
+
+const double _sectionWidth = 120.0;
 
 class AgcBlock extends StatelessWidget {
   const AgcBlock({super.key});
@@ -66,134 +64,149 @@ class AgcBlock extends StatelessWidget {
                         spacing: 2,
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                width: 120,
-                                padding: const EdgeInsets.all(16),
-                                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: context.colorScheme.strokeLight))),
-                                child: FusionAppText(
-                                  text: "THRESHOLD",
-                                  style: context.textTheme.labelMedium?.copyWith(
-                                    color: context.colorScheme.textSecondary,
+                          SizedBox(
+                            width: _sectionWidth,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: double.infinity,
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(border: Border(bottom: BorderSide(color: context.colorScheme.strokeLight))),
+                                  child: Center(
+                                    child: FusionAppText(
+                                      text: "THRESHOLD",
+                                      style: context.textTheme.labelMedium?.copyWith(
+                                        color: context.colorScheme.textSecondary,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Flexible(
-                                child: Padding(
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16),
+                                    child: VerticalSlider(
+                                      value: -10,
+                                      max: 12,
+                                      min: -60,
+                                      onChanged: (num value) {
+                                        print("Threshold changed: $value");
+                                        // context.read<AgcController>().setThreshold(value);
+                                      },
+                                    ),
+                                  ),
+                                ),
+
+                                Container(
                                   padding: const EdgeInsets.all(16),
-                                  child: VerticalSlider(
-                                    value: -10,
-                                    max: 12,
-                                    min: -60,
-                                    onChanged: (num value) {
-                                      print("Threshold changed: $value");
-                                      // context.read<AgcController>().setThreshold(value);
+                                  decoration: BoxDecoration(border: Border(top: BorderSide(color: context.colorScheme.strokeLight))),
+                                  child: NeumorphicGainTextField(
+                                    maxGain: 12,
+                                    minGain: -60,
+                                    onSubmitted: (double value) {
+                                      //
                                     },
                                   ),
                                 ),
-                              ),
-
-                              Container(
-                                padding: const EdgeInsets.all(16),
-                                decoration: BoxDecoration(border: Border(top: BorderSide(color: context.colorScheme.strokeLight))),
-                                child: NeumorphicGainTextField(
-                                  maxGain: 12,
-                                  minGain: -60,
-                                  onSubmitted: (double value) {
-                                    //
-                                  },
-                                ),
-                              ),
-                              FusionAppText(
-                                text: "dBFS",
-                                style: context.textTheme.labelMedium?.copyWith(
-                                  color: context.colorScheme.textSecondary,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                            ],
-                          ),
-                          VerticalDivider(color: context.colorScheme.elevation1, width: 0, thickness: 2),
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                width: 120,
-                                padding: const EdgeInsets.all(16),
-                                decoration: BoxDecoration(
-                                  color: context.colorScheme.elevation2,
-                                  border: Border(bottom: BorderSide(color: context.colorScheme.strokeLight)),
-                                ),
-                                child: FusionAppText(
-                                  text: "REDUCTION",
+                                FusionAppText(
+                                  text: "dBFS",
                                   style: context.textTheme.labelMedium?.copyWith(
                                     color: context.colorScheme.textSecondary,
                                   ),
                                 ),
-                              ),
-                              const Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.all(16.0),
-                                  child: SimpleVerticalMeter(
-                                    value: -10,
-                                    min: -60,
-                                    max: 12,
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                width: 120,
-                                padding: const EdgeInsets.all(16),
-                                decoration: BoxDecoration(
-                                  color: context.colorScheme.elevation2,
-                                  border: Border(top: BorderSide(color: context.colorScheme.strokeLight)),
-                                ),
-                                child: NeumorphicGainTextField(
-                                  controllerValue: 10,
-                                  maxGain: 12,
-                                  minGain: -60,
-                                  onSubmitted: (double value) {
-                                    //
-                                  },
-                                ),
-                              ),
-                              FusionAppText(
-                                text: "dBFS",
-                                style: context.textTheme.labelMedium?.copyWith(
-                                  color: context.colorScheme.textSecondary,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                            ],
+                                const SizedBox(height: 10),
+                              ],
+                            ),
                           ),
                           VerticalDivider(color: context.colorScheme.elevation1, width: 0, thickness: 2),
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                width: 120,
-                                padding: const EdgeInsets.all(16),
-                                decoration: BoxDecoration(
-                                  color: context.colorScheme.elevation2,
-                                  border: Border(bottom: BorderSide(color: context.colorScheme.strokeLight)),
+                          SizedBox(
+                            width: _sectionWidth,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: double.infinity,
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                    color: context.colorScheme.elevation2,
+                                    border: Border(bottom: BorderSide(color: context.colorScheme.strokeLight)),
+                                  ),
+                                  child: Center(
+                                    child: FusionAppText(
+                                      text: "REDUCTION",
+                                      style: context.textTheme.labelMedium?.copyWith(
+                                        color: context.colorScheme.textSecondary,
+                                      ),
+                                    ),
+                                  ),
                                 ),
-                                child: FusionAppText(
-                                  text: "OUTPUT",
+                                const Flexible(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(16.0),
+                                    child: SimpleVerticalMeter(
+                                      value: -10,
+                                      min: -60,
+                                      max: 12,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: 120,
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                    color: context.colorScheme.elevation2,
+                                    border: Border(top: BorderSide(color: context.colorScheme.strokeLight)),
+                                  ),
+                                  child: NeumorphicGainTextField(
+                                    controllerValue: 10,
+                                    maxGain: 12,
+                                    minGain: -60,
+                                    onSubmitted: (double value) {
+                                      //
+                                    },
+                                  ),
+                                ),
+                                FusionAppText(
+                                  text: "dBFS",
                                   style: context.textTheme.labelMedium?.copyWith(
                                     color: context.colorScheme.textSecondary,
                                   ),
                                 ),
-                              ),
-                              const Flexible(
-                                child: Padding(
-                                  padding: EdgeInsets.all(16.0),
-                                  child: VerticalMeter(
-                                    min: -60,
-                                    max: 0,
-                                    value: -10,
+                                const SizedBox(height: 10),
+                              ],
+                            ),
+                          ),
+                          VerticalDivider(color: context.colorScheme.elevation1, width: 0, thickness: 2),
+                          SizedBox(
+                            width: _sectionWidth,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: double.infinity,
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                    color: context.colorScheme.elevation2,
+                                    border: Border(bottom: BorderSide(color: context.colorScheme.strokeLight)),
+                                  ),
+                                  child: Center(
+                                    child: FusionAppText(
+                                      text: "OUTPUT",
+                                      style: context.textTheme.labelMedium?.copyWith(
+                                        color: context.colorScheme.textSecondary,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                const Flexible(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(16.0),
+                                    child: VerticalMeter(
+                                      min: -60,
+                                      max: 0,
+                                      value: -10,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
