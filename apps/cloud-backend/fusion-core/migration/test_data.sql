39e38e96d9bfa554bc52fd116c85ffe6e6658858
sudheesh-BoseProfessional <sudheesh.b@boseprofessional.com>
2026-02-23 15:34:44 +0530

    Adding auth to product

--- apps/cloud-backend/fusion-core/migration/test_data.sql
+++ apps/cloud-backend/fusion-core/migration/test_data.sql
@@ -67,7 +67,9 @@ INSERT INTO feature (id, name, description) VALUES
   (16,'project.start','Can star a project'),
   (17,'project.unstar','Can unstar a project'),
   (18,'project.assign_user','Can assign a user to a project'),
-  (19,'project.remove_user','Can remove a user from a project');
+  (19,'project.remove_user','Can remove a user from a project')
+  (20,'product.read','Can view product catalog and details')
+  ;
 
 INSERT INTO feature_permission (id, feature_id, account_type_role_id, access_level_id, created_at) VALUES
   -- project_file.create
