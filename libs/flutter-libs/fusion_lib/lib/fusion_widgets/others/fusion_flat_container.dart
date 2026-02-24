624e6d32b52f84c85e09dbd771ded0c7142ae9a5
Rithesh Salyan <rithesh.salyan@boseprofessional.com>
2026-02-24 10:18:53 +0530

    added flat container

---
+++ libs/flutter-libs/fusion_lib/lib/fusion_widgets/others/fusion_flat_container.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:fusion_lib/fusion_lib.dart';
+
+class FusionFlatContainer extends StatelessWidget {
+  const FusionFlatContainer({
+    super.key,
+    this.width,
+    this.borderRadius = 12.0,
+    required this.child,
+    this.semanticsId,
+    this.color,
+    this.borderColor,
+    this.height,
+    this.alignment,
+    this.padding,
+  });
+  final double? width;
+  final double? height;
+  final double borderRadius;
+  final Widget child;
+  final Color? color;
+  final Color? borderColor;
+  final AlignmentGeometry? alignment;
+  final EdgeInsetsGeometry? padding;
+  final String? semanticsId;
+  @override
+  Widget build(BuildContext context) {
+    return SemanticHelper.container(
+      testId: SemanticHelper.createTestId(SemanticTypes.container, "fusion_flat_container_${semanticsId ?? ""}"),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 100),
+        width: width,
+        height: height,
+        alignment: alignment,
+        margin: const EdgeInsets.all(2),
+        padding: padding ?? EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: color ?? context.colorScheme.elevation1,
+
+          borderRadius: BorderRadius.circular(borderRadius),
+          border: Border.all(color: borderColor ?? context.colorScheme.elevation2, width: 1),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(borderRadius),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
