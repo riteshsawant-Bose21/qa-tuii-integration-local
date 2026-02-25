c7171e8d4a025afacd70b65e70c66ebb47c27326
Bhuvana-BoseProfessional <Bhuvana.Odiyoor@boseprofessional.com>
2026-02-25 17:05:21 +0530

    removed usecases and entity part

--- apps/frontend/flutter-apps/fusion_web/lib/features/projects/domain/usecases/archive_project_usecase.dart
+++
@@ -1,3 +0,0 @@
-// import 'package:fusion_web/core/usecases/usecase.dart';
-// import 'package:fusion_web/features/projects/domain/entities/project_entity.dart';
-// import 'package:fusion_web/features/projects/domain/repositories/projects_repository.dart';
--- apps/frontend/flutter-apps/fusion_web/lib/features/projects/domain/usecases/useCases.dart
+++
@@ -1,7 +0,0 @@
-// export 'get_projects_usecase.dart';
-// export 'get_project_by_id_usecase.dart';
-// export 'create_project_usecase.dart';
-// export 'update_project_usecase.dart';
-// export 'delete_project_usecase.dart';
-// export 'search_projects_usecase.dart';
-// export 'archive_project_usecase.dart';
