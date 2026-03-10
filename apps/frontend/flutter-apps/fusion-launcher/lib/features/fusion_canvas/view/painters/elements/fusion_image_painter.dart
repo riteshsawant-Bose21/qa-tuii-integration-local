1ef6f439732bdfd45795f714dbaf283c88af1e68
Rithesh Salyan <rithesh.salyan@boseprofessional.com>
2026-03-10 17:24:10 +0530

    WIP:  - Spl data

--- apps/frontend/flutter-apps/fusion-launcher/lib/features/fusion_canvas/view/painters/elements/fusion_image_painter.dart
+++ apps/frontend/flutter-apps/fusion-launcher/lib/features/fusion_canvas/view/painters/elements/fusion_image_painter.dart
@@ -15,13 +15,16 @@ class FusionImagePainter extends FusionBasePainter {
     required this.size,
   });
 
+  Rect getRect() {
+    return Rect.fromLTWH(position.dx, position.dy, size.width, size.height);
+  }
+
   @override
   void paint(Canvas canvas, Size size, FusionCanvasPainter painter) {
-    final Rect dstRect = Rect.fromLTWH(position.dx, position.dy, this.size.width, this.size.height);
     drawImage(
       canvas: canvas,
       imagePath: image,
-      rect: dstRect,
+      rect: getRect(),
       painter: painter,
     );
   }
