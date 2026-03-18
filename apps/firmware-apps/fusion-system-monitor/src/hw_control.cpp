46ed24c88add3f7b51dcf0258c87f10fd282cb5f
Nathan Mark <nathan.mark@boseprofessional.com>
2026-03-18 13:02:00 -0700

    fixing php default polarity

--- apps/firmware-apps/fusion-system-monitor/src/hw_control.cpp
+++ apps/firmware-apps/fusion-system-monitor/src/hw_control.cpp
@@ -261,7 +261,7 @@ HWControl::HWControl(const bosepro::BlockConfiguration &configuration)
     if (!php_led_name.empty()) {
         php_led_chip_path = discover_pwm_chip_path(php_led_name);
         if (!php_led_chip_path.empty()) {
-            set_pwm_output(php_led_chip_path, kPhpLedPwmChannel, false);
+            set_pwm_output(php_led_chip_path, kPhpLedPwmChannel, true);
         }
     }
 
