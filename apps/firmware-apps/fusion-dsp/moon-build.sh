15f87ad5cf0b395b3342452def59ba1c94c8cb6e
Amol-Kirtane-Boseprofessional <153024044+Amol-Kirtane-Boseprofessional@users.noreply.github.com>
2026-03-27 14:00:05 -0400

    build: Try updated yocto SDK for build

--- apps/firmware-apps/fusion-dsp/moon-build.sh
+++ apps/firmware-apps/fusion-dsp/moon-build.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 # Build the fusion-dsp binary in a single moon task.
 
-source /home/ec2-user/fusion-nano-build-cache/bose/fusion-nano/yocto-sdk/environment-setup-armv8a-poky-linux
+source /home/ec2-user/fusion-mini-build-cache/bose/fusion/yocto-sdk/environment-setup-cortexa53-crypto-poky-linux
 python3 ./waf configure --platform=varmini
 python3 ./waf build
--- apps/firmware-apps/fusion-telemetry-core/moon-build.sh
+++ apps/firmware-apps/fusion-telemetry-core/moon-build.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 # Build the fusion-dsp binary in a single moon task.
 
+export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 source /home/ec2-user/fusion-nano-build-cache/bose/fusion-nano/yocto-sdk/environment-setup-armv8a-poky-linux
 python3 ./waf configure --platform=varmini
 python3 ./waf build
