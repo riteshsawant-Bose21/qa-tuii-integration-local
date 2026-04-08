b1b4e2bcd19cb748ed2fa6c4bccab035bc1da99e
SidharthSuvarna-BoseProfessional <sidharth.suvarna@boseprofessional.com>
2026-04-08 18:37:42 +0530

    refactor

--- apps/cloud-backend/fusion-core/internal/fusion/device/db/service.go
+++ apps/cloud-backend/fusion-core/internal/fusion/device/db/service.go
@@ -45,7 +45,7 @@ func (s *Service) GetDeviceByID(ctx context.Context, deviceID string, logger *za
 
 	// Guard against empty deviceID to prevent unnecessary DB query
 	if deviceID == "" {
-		return nil, fmt.Errorf(errorutil.ErrMsgDeviceIDEmpty)
+		return nil, fmt.Errorf("GetDeviceByID: %w", errorutil.ErrMsgDeviceIDEmpty)
 	}
 
 	device, err := models.Devices(models.DeviceWhere.SerialNumber.EQ(deviceID)).One(ctx, s.db)
