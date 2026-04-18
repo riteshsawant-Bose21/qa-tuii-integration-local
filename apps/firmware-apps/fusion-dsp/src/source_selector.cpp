9b5726f0b82f3c1baffc652a05d288ba79fb4e72
Nathan Mark <nathan.mark@boseprofessional.com>
2026-04-17 20:44:00 -0700

    fixing bug around selector parameter setting recall on reboot

--- apps/firmware-apps/fusion-dsp/src/source_selector.cpp
+++ apps/firmware-apps/fusion-dsp/src/source_selector.cpp
@@ -1,6 +1,7 @@
 
 #include <bosepro/algorithm.h>
 
+#include <atomic>
 #include <cstdint>
 #include <cstring>
 
@@ -16,9 +17,12 @@ public:
     virtual void process() override;
 
 private:
+    void update_selected_input();
+
     int_fast32_t source_channels;
     int num_inputs;
-    int_fast32_t selected_input = 1;
+    int_fast32_t requested_input = 1;
+    std::atomic<int_fast32_t> active_input{1};
     bosepro::DspSignalMemory<const float *[]> in;
     bosepro::DspSignalMemory<float *[]> out;
 
@@ -43,19 +47,54 @@ SourceSelector::SourceSelector(const bosepro::BlockConfiguration &configuration)
     assign_terminal("in", in);
     assign_terminal("out", out);
 
-    assign_parameter("input", &selected_input);
+    assign_parameter("input", &requested_input, POST_FUNCTION_SCALAR(update_selected_input));
+    update_selected_input();
+}
+
+
+void SourceSelector::update_selected_input()
+{
+    const int max_inputs = (source_channels > 0) ? (num_inputs / source_channels) : 0;
+    int_fast32_t clamped_input = requested_input;
+
+    if (max_inputs <= 0)
+    {
+        clamped_input = 1;
+    }
+    else if (clamped_input < 1)
+    {
+        clamped_input = 1;
+    }
+    else if (clamped_input > max_inputs)
+    {
+        clamped_input = max_inputs;
+    }
+
+    active_input.store(clamped_input, std::memory_order_release);
 }
 
 
 void SourceSelector::process()
 {
-    int start_channel = (selected_input - 1) * source_channels;
+    const int selected_input = static_cast<int>(active_input.load(std::memory_order_acquire));
+    const int max_inputs = (source_channels > 0) ? (num_inputs / source_channels) : 0;
+
+    if (selected_input < 1 || selected_input > max_inputs)
+    {
+        for (int_fast32_t channel = 0; channel < source_channels; channel++)
+        {
+            std::memset(out[channel], 0, get_frame_size() * sizeof(float));
+        }
+        return;
+    }
+
+    const int start_channel = (selected_input - 1) * source_channels;
 
     for (int_fast32_t channel = 0; channel < source_channels; channel++)
     {
         std::memcpy(out[channel], in[start_channel + channel],
                     get_frame_size() * sizeof(float));
-    }   
+    }
 }
 
 
