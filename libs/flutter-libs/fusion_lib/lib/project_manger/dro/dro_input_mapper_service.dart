c018c22a79fcae926eed7006ad6429676807c669
MaheshGouda-BoseProfessional <mahesh.gouda@boseprofessional.com>
2026-04-19 16:50:46 +0530

    feat: aes67 input stream data added to Dro

--- libs/flutter-libs/fusion_lib/lib/project_manger/dro/dro_input_mapper_service.dart
+++ libs/flutter-libs/fusion_lib/lib/project_manger/dro/dro_input_mapper_service.dart
@@ -59,10 +59,45 @@ extension DroInputMapperService on ProjectService {
   }
 
   DroInputModel updateInputStreamData(DroInputModel droInputModel) {
+    List<DroInputStream> inputStreams = [];
+
+    List<AssignedInputStreamInfo> inputStreamInfo = getAssignedInputStreamChannelsForSource();
+
+    for (AssignedInputStreamInfo streamInfo in inputStreamInfo) {
+      if (streamInfo.sourceMappings.isEmpty) {
+        continue;
+      }
+      List<StreamChannelMapping> streamChannelMapping = [];
+      for (StreamSourceChannelMapping sourceMapping in streamInfo.sourceMappings) {
+        if (sourceMapping.channelNumbers.isEmpty) {
+          continue;
+        }
+        streamChannelMapping.add(
+          StreamChannelMapping(
+            ioId: sourceMapping.sourceId,
+            ioCh: sourceMapping.channelNumbers.first,
+          ),
+        );
+      }
+      if (streamChannelMapping.isEmpty) {
+        continue;
+      }
+      inputStreams.add(
+        DroInputStream(
+          id: streamInfo.streamId,
+          name: streamInfo.streamName,
+          serverLocation: '',
+          totalChannels: streamInfo.sourceMappings.length,
+          sourcePort: 5004,
+          multicastDestinationIp: streamInfo.ipAddress,
+          streamChannelMapping: streamChannelMapping,
+        ),
+      );
+    }
     //Update Input Stream Data
     droInputModel = droInputModel.copyWith(
       //input steam not implemented yet, so we will use empty list for now
-      inputStreams: [],
+      inputStreams: inputStreams,
     );
     return droInputModel;
   }
@@ -498,6 +533,43 @@ extension DroInputMapperService on ProjectService {
   //Update output stream data
   DroInputModel updateOutputStreamData(DroInputModel droInputModel) {
     List<DroOutputStream> droOutputStreams = [];
+
+    List<AssignedOutputStreamInfo> outputStreams = getAssignedOutputStreamToCircuit();
+
+    for (AssignedOutputStreamInfo streamInfo in outputStreams) {
+      if (streamInfo.assignedCircuits.isEmpty) {
+        continue;
+      }
+
+      List<StreamChannelMapping> streamChannelMapping = [];
+
+      for (AssignedCircuitInfo circuitMapping in streamInfo.assignedCircuits) {
+        if (circuitMapping.channels.isEmpty) {
+          continue;
+        }
+        streamChannelMapping.add(
+          StreamChannelMapping(
+            ioId: circuitMapping.circuitId,
+            ioCh: circuitMapping.channels.first.channelNumber,
+          ),
+        );
+      }
+
+      if (streamChannelMapping.isEmpty) {
+        continue;
+      }
+
+      droOutputStreams.add(
+        DroOutputStream(
+          id: streamInfo.streamId,
+          name: streamInfo.streamName,
+          serverLocation: '',
+          sourcePort: 49152,
+          multicastDestinationIp: streamInfo.ipAddress,
+          streamChannelMapping: streamChannelMapping,
+        ),
+      );
+    }
 
     //No output steam implemented yet, so we will use empty list for now
 
