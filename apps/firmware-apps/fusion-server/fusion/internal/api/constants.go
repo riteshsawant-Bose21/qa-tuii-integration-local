379a5ec4be9dbeb4408d42a8e83815038f7fa6c3
SidharthSuvarna-BoseProfessional <sidharth.suvarna@boseprofessional.com>
2026-04-23 19:27:01 +0530

    resolving PR comment to update path for audio files and persistance to /persist path

--- apps/firmware-apps/fusion-server/fusion/internal/api/constants.go
+++ apps/firmware-apps/fusion-server/fusion/internal/api/constants.go
@@ -123,7 +123,7 @@ const (
 )
 
 var (
-	AudioFilesLocation      = getenvDefault("FUSION_AUDIO_DIR", "/var/lib/fusion/audio")
+	AudioFilesLocation      = getenvDefault("FUSION_AUDIO_DIR", "/persist/fusion/audio")
 	DefaultIdentityFilePath = getenvDefault("FUSION_IDENTITY_DIR", "/persist/pki/")
 	DefaultCAFileName       = "AmazonRootCA1.pem"
 	DefaultCSRFileName      = "device.csr"
