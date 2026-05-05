a424f3532b5dacde1c61145f70556b254e8544ce
VibhanshuJain-BoseProfessional <vibhanshu.jain@boseprofessional.com>
2026-05-05 14:26:31 -0500

    removed valid model names

--- apps/firmware-apps/fusion-server/fusion/internal/server/server_manufacturing.go
+++ apps/firmware-apps/fusion-server/fusion/internal/server/server_manufacturing.go
@@ -13,16 +13,6 @@ import (
 	"fusion/internal/utils"
 )
 
-var validModelNames = map[string]bool{
-	"c1-evk":     true,
-	"powersmart": true,
-	"fm6":        true,
-	"fm8y":       true,
-	"xlr-pal":    true,
-	"blue-pal":   true,
-	"som":        true,
-}
-
 func (s *FusionServer) SetModelNameLocal(w http.ResponseWriter, r *http.Request) {
 	if !utils.RequirePost(w, r) {
 		return
@@ -32,11 +22,6 @@ func (s *FusionServer) SetModelNameLocal(w http.ResponseWriter, r *http.Request)
 	var req api.SetModelNameRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, fmt.Sprintf("Invalid JSON: %v", err), http.StatusBadRequest)
-		return
-	}
-
-	if !validModelNames[req.ModelName] {
-		http.Error(w, fmt.Sprintf("invalid model name %q: must be one of c1-evk, powersmart, fm6, fm8y, xlr-pal, blue-pal, som", req.ModelName), http.StatusBadRequest)
 		return
 	}
 
